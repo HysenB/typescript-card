5953b0e714cfba0c7520f7b9874aabf90b9b5c07
HysenB <hysi2000@live.com>
2023-03-18 18:17:12 +0100

    useCartContext

--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -1,3 +1,5 @@
+import { useMemo, useReducer } from "react"
+
 export type CartItemType = {
     sku: string,
     name: string,
@@ -39,11 +41,29 @@ const reducer = (state: CartStateType, action: ReducerAction): CartStateType => 
             if (!action.payload) {
                 throw new Error('Action.payload missing in REMOVE action')
             }
+            const { sku } = action.payload
+            const fileteredCard: CartItemType[] = state.cart.filter(item => item.sku !== sku)
+
+            return { ...state, cart: [...fileteredCard] }
         }
         case REDUCER_ACTION_TYPE.QUANTITY: {
             if (!action.payload) {
                 throw new Error('Action.payload missing in QUANTITY action')
             }
+
+            const { sku, qty } = action.payload
+
+            const itemExists: CartItemType | undefined = state.cart.find(item => item.sku === sku)
+
+            if (!itemExists) {
+                throw new Error('Item must exist in order to update quantity');
+            }
+
+            const updatedItem: CartItemType = { ...itemExists, qty };
+
+            const fileteredCard: CartItemType[] = state.cart.filter(item => item.sku !== sku)
+
+            return { ...state, cart: [...fileteredCard, updatedItem] }
         }
         case REDUCER_ACTION_TYPE.SUBMIT: {
             return { ...state, cart: [] }
@@ -51,4 +71,31 @@ const reducer = (state: CartStateType, action: ReducerAction): CartStateType => 
         default:
             throw new Error('Unidentified reducer action type')
     }
+}
+
+const useCartContext = (initCartState: CartStateType) => {
+    const [state, dispatch] = useReducer(reducer, initCartState);
+
+    const REDUCER_ACTIONS = useMemo(() => {
+        return REDUCER_ACTION_TYPE
+    }, [])
+
+    const totalItems: number = state.cart.reduce((previousValue, cartItem) => {
+        return previousValue + cartItem.qty
+    }, 0)
+
+    const totalPrice = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(
+        state.cart.reduce((previousValue, cartItem) => {
+            return previousValue + (cartItem.qty * cartItem.price)
+        }, 0)
+    )
+
+    const cart = state.cart.sort((a, b) => {
+        const itemA = Number(a.sku.slice(-4))
+        const itemB = Number(b.sku.slice(-4))
+        return itemA - itemB;
+    })
+
+    return { dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart }
+
 }
